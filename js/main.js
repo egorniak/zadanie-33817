a629a2f6625115552541f498730f0f78f9364e93
egorniak <ewagorniak@op.pl>
2022-01-16 16:11:54 +0100

    wykonane zadanie

---
+++ js/main.js
@@ -0,0 +1,13 @@
+console.log(`WARSZTAT - Ustaw tło elementów HTML`);
+
+const setBackground = () => {
+    let p1 = document.querySelector('.first');
+    let p2 = document.querySelector('.last');
+   
+    p1.classList.add('bg-red');
+    p2.classList.add('bg-yellow');
+}
+
+let btnSetBackground = document.getElementById('set-background')
+
+btnSetBackground.addEventListener('click',setBackground);
